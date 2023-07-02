e1b3423a2da14b017b0d9f3493af2eeaacb878bd
bltszr <alternate.3401@gmail.com>
2023-07-03 01:51:32 +0700

    UNVERIFIED: Modified function to handle OOMs, not sure if it actually works

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 yolo_model = torch.hub.load('ultralytics/yolov5', 'custom', 'yolo.pt')
 ocr_decoder = CTCDecoder.from_path('ocr_state.pt', CTCCRNNNoStretchV2, CHAR_MAP, blank_char=PEGON_CHARS[0])
 ocr_decoder.model = ocr_decoder.model.to(DEVICE)
+transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    ResizeAndPadHorizontal(target_h=ocr_decoder.model.image_height, target_w=ocr_decoder.model.image_width),
+    transforms.RandomHorizontalFlip(p=1),
+    transforms.ToTensor(),
+])
 
 @app.get("/ping")
 async def ping():
@@ -29,27 +35,28 @@ async def detect_objects(file: UploadFile = File(...)):
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes))
     # Perform object detection
-    result = yolo_model(image).pandas().xyxy[0]
-    result = result.sort_values('ymin')
-
-    transform = transforms.Compose([
-                          transforms.Grayscale(num_output_channels=1),
-                          ResizeAndPadHorizontal(target_h=ocr_decoder.model.image_height, target_w=ocr_decoder.model.image_width),
-                          transforms.RandomHorizontalFlip(p=1),
-                          transforms.ToTensor(),
-                      ])
-
+    result = []
+    try:
+        result = yolo_model(image).pandas().xyxy[0]
+        result = result.sort_values('ymin')
+    except RuntimeError:
+        return {'result': [], 'message': "Failed to detect text: Out of memory"}
+    if result.empty:
+        return {'result': [], 'message': "Detected no text at all"}
+    # we shouldn't need to check if the result variable is empty at this point
     line_imgs = []
     for index, row in result.iterrows():
-    # Access the column values of each row
+        # Access the column values of each row
         if row['confidence'] > 0.5:
             line_img = image.crop((row['xmin'], row['ymin'], row['xmax'], row['ymax']))
             line_imgs.append(transform(line_img))
     line_imgs = torch.stack(line_imgs).to(DEVICE)
-
-    result = evaluate(ocr_decoder, line_imgs)
-
-    return {'result': result}
+    
+    try:
+        result = evaluate(ocr_decoder, line_imgs)
+        return {'result': result}
+    except RuntimeError:
+        return {'result': [], 'message': "Failed to recognize: Out of memory"}
 
 
 def evaluate(decoder, data):
